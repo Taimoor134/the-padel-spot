425bc0a9e6f2135914dc39ed8ec2ec9ca65ee04f
Taimoor134 <taimoor134@gmail.com>
2025-07-13 16:50:58 +0500

    Add padel page

--- pages/padel.js
+++ pages/padel.js
@@ -1,5 +1,6 @@
+// pages/padel.js
 import PadelBookingPrototype from '../components/PadelBookingPrototype';
 
 export default function PadelPage() {
-  return <h1>Welcome to the Padel Booking Page</h1>;
+  return <div>Hello Padel Page</div>;
 }
