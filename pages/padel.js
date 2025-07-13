767e69165bd0b7fe158c5583f7051df44d7203d8
Taimoor134 <taimoor134@gmail.com>
2025-07-13 16:58:04 +0500

    Link Padel page to booking prototype component

--- pages/padel.js
+++ pages/padel.js
@@ -1,4 +1,4 @@
-import PadelBookingPrototype from '../components/PadelBookingPrototype';
+import PadelBookingPrototype from "@/components/PadelBookingPrototype";
 
 export default function PadelPage() {
   return <PadelBookingPrototype />;
