f211982892755dd2dccae81f84fa6de6350b389b
Taimoor134 <taimoor134@gmail.com>
2025-07-13 16:28:34 +0500

    Add basic PadelBookingPrototype component

---
+++ components/PadelBookingPrototype.js
@@ -0,0 +1,12 @@
+// components/PadelBookingPrototype.js
+
+import React from 'react';
+
+export default function PadelBookingPrototype() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Padel Booking Prototype</h1>
+      <p>This is a placeholder component for the booking system.</p>
+    </div>
+  );
+}
